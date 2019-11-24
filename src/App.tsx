7aa114a8f466e89b4d03e1b5f3c1d67b21ab07a5
Jamie Haywood <jamie.haywood@gmail.com>
2019-11-24 22:59:43 +0000

    added React Router on App tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import './App.css';
+import { Home } from './pages'
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        {/* <Route exact path='/CV' component={Cv} />
+        <Route exact path='/Books' component={Books} /> */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
