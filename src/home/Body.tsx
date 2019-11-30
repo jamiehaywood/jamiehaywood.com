b20bafee69272c596cbb658320c791e1f7713cb9
Jamie Haywood <jamie.haywood@gmail.com>
2019-11-30 20:45:17 +0000

    modified body

--- src/home/Body.tsx
+++ src/home/Body.tsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import './body.css'
 
-interface props {}
+interface props { }
 
-const Body:React.FC<props> = (props) => {
+const Body: React.FC<props> = (props) => {
   return (
-    <div className="container">Hi, my name is Jamie.
-<p/>
-    I'm a software developer at&nbsp;<a className="tfl" href="https://tfl.gov.uk">Transport for London</a> and an Anthropology graduate from <a className="durham" href="https://www.dur.ac.uk">Durham University.</a>
-    <p/>
+    <div className="fadeIn animated delay-three container">
 
-    I believe the value of technology comes from the people who use it. 
-    
-    As such, a human-centric approach needs to be applied to what is increasingly becoming an array of complex and impersonal technologies. With this in mind, I want to apply my anthropological background to software development to build products that adopt this approach and have a lasting impact on how we interact with the world.
-    <p />
-    I love experimenting with new technologies and implementing them into my working environment. I like creating digital and analogue systems to streamline my workflow to achieve maximum productivity.
-    <p/>
-    When I'm not developing software, I enjoy sailing, cooking, playing backgammon, and the piano.
+      <h1> Hi, I'm Jamie. </h1>
+
+      I'm a Full Stack Software Developer at Transport for London&nbsp;
+
+      <img className="roundel" alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/TfL_roundel_%28no_text%29.svg/500px-TfL_roundel_%28no_text%29.svg.png" /> <p />
+
+      <p />
+
+      I believe that the software is best produced using a <b>customer-centric</b> approach and <b>rapid iteration</b> to deliver the most value in the shortest amount of time. As such, I frequently apply my anthropological background to build software products that have a <b>lasting impact</b> on how people interact with them.
+
+      <p />
+
+      I am a big experimenter with new technologies and implementing them into my personal and work projects. Every month I try out a new technology and write about it <a href="/blog" className="blogLink black">here</a>.
+
+      <p />
+
+      When I'm not developing software, I enjoy sailing, cooking, playing backgammon, and reading.
     </div>
-   )
- }
+  )
+}
 
 export default Body
