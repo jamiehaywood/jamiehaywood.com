d14dabdde7c111b36d5e989ea49880f2b24575bc
Jamie Haywood <jamie.haywood@gmail.com>
2019-11-30 20:46:00 +0000

    added navbar and socialbar

---
+++ src/navigation/navbar/NavBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './navbar.css'
+
+interface props {
+}
+
+const TopNav: React.FC<props> = (props) => {
+    return (
+        <div className="navigation fadeIn animated delay-two">
+            <a className="navLinks" href="/blog">BLOG</a>
+            &nbsp; | &nbsp;
+            <a className="navLinks" href="/cv">CV</a>
+            &nbsp; | &nbsp;
+            <a className="navLinks" href="/projects">PROJECTS</a>
+        </div >
+    )
+}
+
+export default TopNav
+
---
+++ src/navigation/socialbar/index.ts
@@ -0,0 +1,6 @@
+import GitHub from './GitHub'
+import LinkedIn from './Linkedin'
+import SocialBar from './SocialBar'
+
+export { GitHub, LinkedIn }
+export default SocialBar
