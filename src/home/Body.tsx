fad13f7278b5dd8a6756b7d595a001bcc89b66cd
Jamie Haywood <jamie.haywood@gmail.com>
2019-12-04 10:12:40 +0000

    tidying up html

--- src/home/Body.tsx
+++ src/home/Body.tsx
@@ -12,23 +12,21 @@ const Body: React.FC<props> = props => {
         className="roundel"
         alt=""
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/TfL_roundel_%28no_text%29.svg/500px-TfL_roundel_%28no_text%29.svg.png"
-      />{" "}
-      <p />
-      <p />I believe that the software is best produced using a{" "}
-      <b>customer-centric</b> approach and <b>rapid iteration</b> to deliver the
-      most value in the shortest amount of time. As such, I frequently apply my
-      anthropological background to build software products that have a{" "}
-      <b>lasting impact</b> on how people interact with them.
-      <p />I am a big experimenter with new technologies and implementing them
+      />
+      <br/>
+      <p>I believe that the best software is produced using a <b>customer-centric</b> approach 
+      and <b>rapid iteration</b> to deliver the most value in the shortest amount of time. 
+      As such, I frequently apply my anthropological background to build software products 
+      that have a <b>lasting impact</b> on how people interact with them.</p>
+      
+      <p>I am a big experimenter with new technologies and implementing them
       into my personal and work projects. Every month I try out a new technology
-      and write about it{" "}
-      <a href="/blog" className="blogLink black">
-        here
-      </a>
-      .
-      <p />
+      and write about it <a href="/blog" className="blogLink black">here</a>.
+      </p>
+      <p>
       When I'm not developing software, I enjoy sailing, cooking, playing
       backgammon, and reading.
+      </p>
     </div>
   );
 };
