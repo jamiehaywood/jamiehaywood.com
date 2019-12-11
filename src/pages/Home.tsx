ca516ca3e6a631a943907cf887597453e0ec0da9
Jamie Haywood <jamie.haywood@gmail.com>
2019-12-11 21:24:07 +0000

    centered home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,13 @@ import { Body, Image } from "../home";
 
 const Home: React.FC = () => {
   return (
-    <div style={{ marginTop: "2vh" }}>
-      <Image />
-      <NavBar />
-      <Body />
-      <Footer />
+    <div style={{width:"100vw", height:"100vh", display:"flex"}}>
+      <div style={{ margin: "auto" }}>
+        <Image />
+        <NavBar />
+        <Body />
+        <Footer />
+      </div>
     </div>
   );
 };
