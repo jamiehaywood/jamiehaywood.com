7a9715690418439b376bb5fd94e9b5af9d610f8d
Jamie Haywood <jamie.haywood@gmail.com>
2020-10-26 09:43:23 +0000

    fix: add alt text to image

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { underConstruction } from '../images'
+import React from "react";
+import { underConstruction } from "../images";
 
-interface props { }
+interface props {}
 
 const Blog: React.FC<props> = (props) => {
   return (
-    <div style={{ display: "flex", height:"100vh" }}>
+    <div style={{ display: "flex", height: "100vh" }}>
       <div style={{ margin: "auto" }}>
-        <img src={underConstruction} width={"300px"} style={{ textAlign: "center" }} />
+        <img
+          src={underConstruction}
+          alt="under-construction"
+          width={"300px"}
+          style={{ textAlign: "center" }}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
